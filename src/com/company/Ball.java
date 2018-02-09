c7783dd0e3d0a3e194f4e8bc60b862474901b03b
Luke Mwila <outlier.developer@gmail.com>
2018-02-09 20:44:53 +0200

    Initial push

---
+++ src/com/company/Ball.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Ball {
+
+    public void bounce(){
+        System.out.println("Bounce");
+    }
+
+}
---
+++ src/com/company/Basketball.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Basketball extends Ball {
+
+    @Override
+    public void bounce(){
+        System.out.println("Basketball bouncing");
+    }
+
+}
+
